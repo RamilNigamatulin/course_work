12ff6fbde788357fe0b369a2fc053527984db61c
RamilNigamatulin <I@rnigamatulin.ru>
2024-04-01 18:33:45 +0500

    Final version

--- project/function.py
+++ project/function.py
@@ -9,22 +9,21 @@ def load_transactions():
         return base
 
 
-def execute_transaction():
+
+def execute_transaction(base_files):
     """ Создает список успешных операций """
-    base = load_transactions()
     good_transactions = []
-    for transaction in base:
+    for transaction in base_files:
         if transaction.get("state") == "EXECUTED":
             good_transactions.append(transaction)
-    return (good_transactions)
+    return good_transactions
 
 
-def replace_in_date():
+def replace_in_date(base_good_files):
     """приводит даты в соответствующий формат и сортирует дату по убыванию"""
-    stocks = execute_transaction()
     good_dates = []
-    for stock in stocks:
-        stock["date"] = stock["date"].replace("T", " ")
+    for stock in base_good_files:
+        stock['date'] = stock['date'].replace("T", " ")
         stock["date"] = stock["date"][0:19]
         date_obj = datetime.strptime(stock["date"], '%Y-%m-%d %H:%M:%S')
         formatted_date = date_obj.strftime('%d.%m.%Y %H:%M:%S')
@@ -42,34 +41,35 @@ def hide_sensitive_info(sorted_files):
             if 'Счет' in account_number:
                 hidden_account_number = account_number[:-16] + ' ' + account_number[-17:-15] +'** **** **** ' + account_number[-4:]
             else:
-                hidden_account_number = account_number[:-12] + ' ' + account_number[-11:-9] + '** **** ' + account_number[-4:]
+                hidden_account_number = account_number[:-12] + ' ' + account_number[-12:-10] + '** **** ' + account_number[-4:]
             item['from'] = hidden_account_number
         if 'to' in item:
             account_number = item['to']
             if 'Счет' in account_number:
-                hidden_account_number = account_number[:-20] + ' **' + account_number[-4:]
+                hidden_account_number = account_number[:-20] + '**' + account_number[-4:]
             else:
-                hidden_account_number = account_number[:-16] + ' **' + account_number[-4:]
+                hidden_account_number = account_number[:-16] + '**' + account_number[-4:]
             item['to'] = hidden_account_number
     return sorted_files
 
 
 def program_launch(sorted_list):
     '''Выводит конечную требуемую информацию'''
-    name = input("Приветствую! Введите количество последних выполненных операций, которое необходимо вывести: ")
-    try:
-        name = int(name)
-    except ValueError:
-        print("Введите текст цифрами. Начните заново!")
-        exit()
-    if name > len(sorted_list):
-        print("Вы ввели недопустимое количество выполненных операций, попробуйте ввести меньшее количество!")
-        exit()
-    else:
-        end_result = ""
-        for i in sorted_list[:name]:
-            source = i.get('from', '  ')
-            end_result += (f"{i['date'][0:11]} {i['description']}\n"
-                           f"{source} -> {i['to']}\n"
-                           f"{i['operationAmount']['amount']} {i['operationAmount']['currency']['name']}\n")
-        return end_result
+    while True:
+        name = input("Приветствую! Введите количество последних выполненных операций, которое необходимо вывести: ")
+        try:
+            name = int(name)
+            if name > len(sorted_list):
+                print("Вы ввели недопустимое количество выполненных операций, попробуйте ввести меньшее количество!")
+            else:
+                break
+        except ValueError:
+            print("Введите текст цифрами. Попробуйте еще раз!")
+    end_result = ""
+    for i in sorted_list[:name]:
+        source = i.get('from', '  ')
+        end_result += (f"{i['date'][0:11]} {i['description']}\n"
+                       f"{source} -> {i['to']}\n"
+                       f"{i['operationAmount']['amount']} {i['operationAmount']['currency']['name']}\n")
+    return end_result
+
