0bf1f1360a92c829b8f90b88a8b584b76ea54243
RamilNigamatulin <163241991+RamilNigamatulin@users.noreply.github.com>
2024-04-01 18:58:01 +0500

    Delete project/main.py

--- project/main.py
+++
@@ -1,9 +0,0 @@
-from function import replace_in_date
-from function import hide_sensitive_info
-from function import program_launch
-
-sorted_files = replace_in_date()
-sorted_list = hide_sensitive_info(sorted_files)
-program_result = program_launch(sorted_list)
-
-print(program_result)
